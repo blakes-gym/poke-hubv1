4a5bb0a9a47bb4a5fc538de18f4f27ff2c24165c
Derick Park <derickmpark@gmail.com>
2020-01-13 15:39:33 -0800

    Format table

--- pages/home.js
+++ pages/home.js
@@ -1,28 +1,62 @@
 import "../styles.scss";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table'
 import Template from "../components/Template.js";
 
 export default function Home() {
   return (
     <Template>
-      <Container>
-        <Row>
-          <Col>#</Col>
-          <Col>Name</Col>
-          <Col>Type</Col>
-          <Col>Total</Col>
-          <Col>HP</Col>
-          <Col>Attack</Col>
-          <Col>Defense</Col>
-          <Col>Sp. Atk</Col>
-          <Col>Sp. Def</Col>
-          <Col>Speed</Col>
-        </Row>
-      </Container>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Total</th>
+            <th>HP</th>
+            <th>Attack</th>
+            <th>Defense</th>
+            <th>Sp. Atk</th>
+            <th>Sp. Def</th>
+            <th>Speed</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </Template>
   )
 }
 
 
+
+// <Container>
+// <Row>
+//   <Col>#</Col>
+//   <Col>Name</Col>
+//   <Col>Type</Col>
+//   <Col>Total</Col>
+//   <Col>HP</Col>
+//   <Col>Attack</Col>
+//   <Col>Defense</Col>
+//   <Col>Sp. Atk</Col>
+//   <Col>Sp. Def</Col>
+//   <Col>Speed</Col>
+// </Row>
+// </Container>
