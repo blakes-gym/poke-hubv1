e258d8c36a6ab882b270291b6c4136e36aeaf659
Derick Park <derickmpark@gmail.com>
2020-01-13 12:22:57 -0800

    Wrap home component within Template

--- pages/home.js
+++ pages/home.js
@@ -2,23 +2,26 @@ import "../styles.scss";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Template from "../components/Template.js";
 
 export default function Home() {
   return (
-  <Container>
-    <Row>
-      <Col>#</Col>
-      <Col>Name</Col>
-      <Col>Type</Col>
-      <Col>Total</Col>
-      <Col>HP</Col>
-      <Col>Attack</Col>
-      <Col>Defense</Col>
-      <Col>Sp. Atk</Col>
-      <Col>Sp. Def</Col>
-      <Col>Speed</Col>
-    </Row>
-  </Container>
+    <Template>
+      <Container>
+        <Row>
+          <Col>#</Col>
+          <Col>Name</Col>
+          <Col>Type</Col>
+          <Col>Total</Col>
+          <Col>HP</Col>
+          <Col>Attack</Col>
+          <Col>Defense</Col>
+          <Col>Sp. Atk</Col>
+          <Col>Sp. Def</Col>
+          <Col>Speed</Col>
+        </Row>
+      </Container>
+    </Template>
   )
 }
 
