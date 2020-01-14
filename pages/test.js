9d381af62aca8e46181c952474b80fd3298d574b
Blake Mason <blakelmason@gmail.com>
2020-01-14 13:43:50 -0800

    graphql setup

---
+++ pages/test.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const test = gql`
+  {
+    pokemon(id: 220) {
+      name
+    }
+  }
+`
+
+export default function Test() {
+  const { loading, err, data } = useQuery(test)
+
+  if (loading) return <div>Loading. . .</div>
+  if (err) return <div>ERROR</div>
+  return <div>this is a test: {data.pokemon.name}</div>
+}
