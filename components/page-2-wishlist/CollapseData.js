051e5f730542c3c08c2716b374d4e6a2a61a8eca
Kat <katherine.wang429@gmail.com>
2020-01-16 10:56:58 -0800

    Add component for moves

--- components/page-2-wishlist/CollapseData.js
+++ components/page-2-wishlist/CollapseData.js
@@ -2,6 +2,7 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import PokemonStats from './PokemonStats.js';
 import StatsGraph from './StatsGraph.js';
+import Moves from './Moves.js';
 
 export default function CollapseData({ pokemon, idx }) {
   return (
@@ -10,6 +11,7 @@ export default function CollapseData({ pokemon, idx }) {
         <div className='dataContainer'>
           <PokemonStats pokemon={pokemon}></PokemonStats>
           <StatsGraph stats={pokemon.stats}></StatsGraph>
+          <Moves />
         </div>
       </Card.Body>
     </Accordion.Collapse>
