cb497380e60310948a0dac2294b8f5ea08eea780
Kat <katherine.wang429@gmail.com>
2020-01-15 18:09:18 -0800

    Restructure card architecture

--- components/page-2-wishlist/PokemonRow_New.js
+++ components/page-2-wishlist/PokemonRow_New.js
@@ -1,27 +1,17 @@
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-import StatsCard from './StatsCard.js';
-import Badge from 'react-bootstrap/Badge';
-import HexByType from '../../hexData.js';
+import PokemonBanner from './PokemonBanner.js';
+import CollapseData from './CollapseData.js';
 import data from '../../data/dummyData.js';
-import TeamMemberData from './PokemonData.js';
-import TeamMemberGraph from './StatsGraph.js';
 
-export default function Team() {
+export default function PokemonRow_New() {
   return (
     <Accordion>
       {data.map((pokemon, idx) => {
         return (
           <Card key={idx}>
-            <StatsCard idx={idx} pokemon={pokemon} />
-            <Accordion.Collapse eventKey={`${idx}`}>
-              <Card.Body>
-                <div className='dataContainer'>
-                  <TeamMemberData pokemon={pokemon}></TeamMemberData>
-                  <TeamMemberGraph stats={pokemon.Stats}></TeamMemberGraph>
-                </div>
-              </Card.Body>
-            </Accordion.Collapse>
+            <PokemonBanner idx={idx} pokemon={pokemon} />
+            <CollapseData idx={idx} pokemon={pokemon} />
           </Card>
         );
       })}
