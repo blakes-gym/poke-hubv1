71a1f958bcc0b86ba5d1d5663a8ecd87e2826272
Derick Park <derickmpark@gmail.com>
2020-01-16 19:13:07 -0800

    Search Bar

--- components/page-1-home/Window.js
+++ components/page-1-home/Window.js
@@ -1,53 +1,141 @@
 import {Table} from 'react-bootstrap';
 import hexData from '../../utils/hexDataDerick';
 import Search from './Search.js';
+import { MDBDataTable } from 'mdbreact';
+import typeImages from '../../utils/typeImages'
+import {FaAngleDown} from 'react-icons/fa'
+import {FaAngleUp} from 'react-icons/fa'
+import {useState} from 'react';
+
+// export default function Window ({pokemon}) {
+//   const data = {
+//     columns: [
+//     {
+//       label: 'Name',
+//       field: 'name',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'Type',
+//       field: 'type1',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'Type',
+//       field: 'type2',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'HP',
+//       field: 'hp',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'Attack',
+//       field: 'atk',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'Defense',
+//       field: 'def',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'SP. Atk',
+//       field: 'spatk',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'SP. Def',
+//       field: 'spdef',
+//       sort: 'asc',
+//       width: 150
+//     },
+//     {
+//       label: 'Speed',
+//       field: 'speed',
+//       sort: 'asc',
+//       width: 150
+//     }],
+//     rows: pokemon
+//   }
+//   return (
+//     <MDBDataTable
+//       bordered
+//       hover
+//       data={data}
+//     />
+//   )
+// }
+
 
 
 export default function Window ({pokemon}) {
+  const [arrow, setArrow] = useState(<FaAngleDown style={{float: 'right'}}/>)
+
+  const handleClick = () => (
+    setArrow(<FaAngleUp style={{float: 'right'}}/>)
+  )
+
   return (
     <div className="d-none d-sm-block">
       <Search />
         <Table bordered hover>
-            <thead className="thead-dark">
+            <thead className="thead-dark" style={{fontSize: '14px'}}>
               <tr>
-                <th style={{borderRightStyle: 'none'}}>#</th>
-                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}></th>
-                <th>Name</th>
-                <th>Type</th>
-                <th>HP</th>
-                <th>Attack</th>
-                <th>Defense</th>
-                <th>Sp. Atk</th>
-                <th>Sp. Def</th>
-                <th>Speed</th>
-                <th>Total</th>
-                <th>Moves</th>
+                <th onClick={handleClick} style={{borderRightStyle: 'none'}}># </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Name </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Type </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>HP </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Attack </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Defense </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Sp. Atk </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Sp. Def </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Speed </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
+                <th style={{borderRightStyle: 'none'}}>Total </th>
+                <th style={{borderLeftStyle:'none', borderRightStyle: 'none'}}>{arrow}</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody >
               {pokemon.map((pokemon, index) => (
-                <tr key={pokemon.id}>
+                <tr key={pokemon.id} style={{textAlign: 'center'}}>
                   <td style={{width:'5%', borderRightStyle: 'none'}}><img style={{height:'2rem', width: '2rem'}} src={pokemon.icon}></img></td>
                   <td style={{width:'5%', borderLeftStyle:'none', borderRightStyle: 'none'}}>{pokemon.id}</td>
-                  <td style={{width:'10%'}}>{pokemon.name}</td>
-                  <td style={{width:'10%'}}>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.name}</td>
+                  <td colspan="2" style={{width:'10%'}}>
                     { pokemon.type2 ? (
-                      <div key={`${pokemon.id} ${pokemon.type1}`} style={{backgroundColor: hexData[pokemon.type1], color:'white', textAlign: 'center'}}>{pokemon.type1}</div>
-                    ) : (
-                      <div>
-                      <div key={`${pokemon.id} ${pokemon.type1}`} style={{backgroundColor: hexData[pokemon.type1], color:'white', textAlign: 'center'}}>{pokemon.type1}</div>
-                      <div key={`${pokemon.id} ${pokemon.type2}`} style={{backgroundColor: hexData[pokemon.type2], color:'white', textAlign: 'center'}}>{pokemon.type2}</div>
+                      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                        <img src={typeImages[pokemon.type1]} style={{width: '30%'}}/>
+                        <img src={typeImages[pokemon.type2]} style={{width: '30%'}}/>
                       </div>
+                    ) : (
+                      <img src={typeImages[pokemon.type1]} style={{width: '30%'}}/>
                     )}
                   </td>
-                  <td style={{width:'10%'}}>{pokemon.hp}</td>
-                  <td style={{width:'10%'}}>{pokemon.atk}</td>
-                  <td style={{width:'10%'}}>{pokemon.def}</td>
-                  <td style={{width:'10%'}}>{pokemon.spatk}</td>
-                  <td style={{width:'10%'}}>{pokemon.spdef}</td>
-                  <td style={{width:'10%'}}>{pokemon.speed}</td>
-                  <td style={{width:'10%'}}>{pokemon.total}</td>
-                  <td style={{width:'5%'}}>Moves</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.hp}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.atk}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.def}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.spatk}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.spdef}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.speed}</td>
+                  <td colspan="2" style={{width:'10%'}}>{pokemon.total}</td>
+                  {/* <td style={{width:'5%'}}>Moves</td> */}
                 </tr>
               ))}
             </tbody>
---
+++ utils/typeImages.js
@@ -0,0 +1,22 @@
+let ImageByType = {
+  normal: 'https://i.imgur.com/mhL564d.png',
+  fire: 'https://i.imgur.com/n3IWBGr.png',
+  water: 'https://i.imgur.com/35uCgw1.png',
+  electric: 'https://i.imgur.com/NePYiWf.png',
+  grass: 'https://i.imgur.com/CyI0Jrr.png',
+  ice: 'https://i.imgur.com/RWuY5mv.png',
+  fighting: 'https://i.imgur.com/1KR8D1D.png',
+  poison: 'https://i.imgur.com/ScwIsR2.png',
+  ground: 'https://i.imgur.com/Mckzo2I.png',
+  flying: 'https://i.imgur.com/ugHXaQk.png',
+  psychic: 'https://i.imgur.com/rWsJjjj.png',
+  bug: 'https://i.imgur.com/kN1CBZB.png',
+  rock: 'https://i.imgur.com/vb2DI7Q.png',
+  ghost: 'https://i.imgur.com/CBUCtjg.png',
+  dragon: 'https://i.imgur.com/RBg70f7.png',
+  dark: 'https://i.imgur.com/wHlaOVy.png',
+  steel: 'https://i.imgur.com/z3FPqfC.png',
+  fairy: 'https://i.imgur.com/9j3oe1Y.png' 
+}
+
+export default ImageByType;
