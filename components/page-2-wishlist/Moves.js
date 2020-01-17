71ab2e06c1a77bb401def5f87b39192873aae0b7
Kat <katherine.wang429@gmail.com>
2020-01-16 18:06:04 -0800

    Add four move dropdowns

--- components/page-2-wishlist/Moves.js
+++ components/page-2-wishlist/Moves.js
@@ -1,13 +1,52 @@
-import MoveSelection from './MoveSelection.js';
-import allPokemonData from '../../data/dummyData.js';
+import { Component } from 'react';
+import moveData from '../../data/dummyData2.js';
+class Moves extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      move1: null,
+      move2: null,
+      move3: null,
+      move4: null
+    };
 
-export default function Moves() {
-  return (
-    <div>
-      <MoveSelection allPokemon={allPokemonData} />
-      <MoveSelection allPokemon={allPokemonData} />
-      <MoveSelection allPokemon={allPokemonData} />
-      <MoveSelection allPokemon={allPokemonData} />
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      },
+      console.log(this.state)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {Object.keys(this.state).map((moveNo, index) => {
+          return (
+            <div key={index}>
+              <form>
+                <select name={moveNo} onChange={this.handleChange}>
+                  {moveData.moves.map((moveDetails, index) => {
+                    return (
+                      <option value={moveDetails.move} key={index}>
+                        {moveDetails.move}
+                      </option>
+                    );
+                  })}
+                </select>
+              </form>
+              <div>Dmg:</div>
+              <div>PP:</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
+
+export default Moves;
