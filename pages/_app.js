7d468461dc5fe7f0fede8195992f8749890e0762
Blake Mason <blakelmason@gmail.com>
2020-01-16 16:43:21 -0800

    remove _app.scss

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import { withApollo } from '../graphql/apollo'
 
 import Template from '../components/Template'
 
-import './_app.scss'
-
 function _App({ Component, pageProps }) {
   return (
     <Template>
